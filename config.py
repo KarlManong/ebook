707f672a3a5c74659b5a186b57ada47b7133f7a1
pocari-- <eggsspamham@gmail.com>
2014-11-22 21:58:27 +0800

    修改一下Linux下db文件和下载目录路径

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+import os
+import platform
+import sys
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///D:\project\ebook\ebooks.db"
 SQLALCHEMY_ECHO = False
-STORAGE_DIR = r"D:\project\ebook\books"
+
+if DEBUG and platform.system() == 'Linux':
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(cwd, "ebooks.db")
+    STORAGE_DIR = os.path.join(cwd, "books")
+else:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///D:\project\ebook\ebooks.db"
+    STORAGE_DIR = r"D:\project\ebook\books"
